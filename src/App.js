984b7a55fd2a6cd942641a09299b988e4545b83a
NeverEvi <thhjp.dekimasen@gmail.com>
2024-03-28 02:31:15 -0400

    add css framework, fix logo imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import twitchLogo from './image/twitch.svg'
+import xLogo from './image/X.svg'
+import facebookLogo from './image/facebook.svg';
+import githubLogo from './image/github.svg'
+import pokeLogo from './image/Pokeball.svg'
+import linkedin from './image/Linkedin.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className="head">
+  <h1>NeverEvi</h1>
+    <div className="links">
+      <div className="pageLink">
+        <a href="https://neverevi.github.io/final-project-frontend/#/" target="_blank" rel="noopener noreferrer">
+          <img src={pokeLogo} className="logo" alt="Pokecard logo" />
         </a>
-      </header>
+        <p>PokeCard</p>
+      </div>
+      <div className="pageLink">
+        <a href="https://github.com/NeverEvi" target="_blank" rel="noopener noreferrer">
+          <img src={githubLogo} className="logo" alt="Github logo" />
+        </a>
+        <p>GitHub</p>
+      </div>
+      <div className="pageLink">
+        <a href="https://www.facebook.com/NeverEvi" target="_blank" rel="noopener noreferrer">
+          <img src={facebookLogo} className="logo" alt="Facebook logo" />
+        </a>
+        <p>Facebook</p>
+      </div>
+      <div className="pageLink">
+        <a href="https://twitter.com/NeverEvi" target="_blank" rel="noopener noreferrer">
+          <img src={xLogo} className="logo" alt="X logo" />
+        </a>
+        <p>Twitter/X</p>
+      </div>
+        <div className="pageLink">
+        <a href="https://www.twitch.tv/neverevi" target="_blank" rel="noopener noreferrer">
+          <img src={twitchLogo} className="logo" alt="Twitch logo" />
+        </a>
+        <p>Twitch.tv</p>
+      </div>
+      <div className="pageLink">
+        <a href="https://www.linkedin.com/in/kane-cramer-neverevi" target="_blank" rel="noopener noreferrer">
+          <img src={linkedin} className="logo" alt="LinkedIn logo" />
+        </a>
+        <p>LinkedIn</p>
+      </div>
+    </div>
+  </div>
+        
+      
     </div>
   );
 }
